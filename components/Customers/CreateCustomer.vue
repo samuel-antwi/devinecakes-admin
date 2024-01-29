fa5b69d09a05ec91a5afd714b5c68bd5e36f306f
Samuel Antwi <samuelantwidev@gmail.com>
2024-01-29 09:12:35 +0000

    Enabled ssr

--- components/Customers/CreateCustomer.vue
+++ components/Customers/CreateCustomer.vue
@@ -4,52 +4,54 @@ import { useCreateCustomer } from "@/components/App/composables/createCustomer";
 const { formData } = useCreateCustomer();
 </script>
 <template>
-  <div class="max-w-2xl">
-    <form class="flex flex-col space-y-10">
-      <div>
-        <label class="text-xl font-medium mb-2 block" for="first name"
-          >First Name *</label
-        >
-        <UInput
-          v-model="formData.firstName"
-          :ui="{ spacing: 'py-4' }"
-          id="first name"
-          size="xl"
-        />
-      </div>
-      <div>
-        <label class="text-xl font-medium mb-2 block" for="surname"
-          >Surname</label
-        >
-        <UInput
-          v-model="formData.surname"
-          :ui="{ spacing: 'py-4' }"
-          id="surname"
-          size="xl"
-        />
-      </div>
-      <div>
-        <label class="text-xl font-medium mb-2 block" for="mobile"
-          >Mobile Number *</label
-        >
-        <UInput
-          v-model="formData.mobileNumber"
-          :ui="{ spacing: 'py-4' }"
-          id="mobile"
-          size="xl"
-        />
-      </div>
-      <div>
-        <label class="text-xl font-medium mb-2 block" for="location"
-          >Location</label
-        >
-        <UInput
-          v-model="formData.location"
-          :ui="{ spacing: 'py-4' }"
-          id="location"
-          size="xl"
-        />
-      </div>
-    </form>
-  </div>
+  <ClientOnly>
+    <div class="max-w-2xl">
+      <form class="flex flex-col space-y-10">
+        <div>
+          <label class="text-xl font-medium mb-2 block" for="first name"
+            >First Name *</label
+          >
+          <UInput
+            v-model="formData.firstName"
+            :ui="{ spacing: 'py-4' }"
+            id="first name"
+            size="xl"
+          />
+        </div>
+        <div>
+          <label class="text-xl font-medium mb-2 block" for="surname"
+            >Surname</label
+          >
+          <UInput
+            v-model="formData.surname"
+            :ui="{ spacing: 'py-4' }"
+            id="surname"
+            size="xl"
+          />
+        </div>
+        <div>
+          <label class="text-xl font-medium mb-2 block" for="mobile"
+            >Mobile Number *</label
+          >
+          <UInput
+            v-model="formData.mobileNumber"
+            :ui="{ spacing: 'py-4' }"
+            id="mobile"
+            size="xl"
+          />
+        </div>
+        <div>
+          <label class="text-xl font-medium mb-2 block" for="location"
+            >Location</label
+          >
+          <UInput
+            v-model="formData.location"
+            :ui="{ spacing: 'py-4' }"
+            id="location"
+            size="xl"
+          />
+        </div>
+      </form>
+    </div>
+  </ClientOnly>
 </template>
