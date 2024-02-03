25962fa82a93637f56182c501a6c75d922d09eaa
Samuel Antwi <samuelantwidev@gmail.com>
2024-02-03 10:23:20 +0000

    Disable ssr for debugging

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import path from "path";
 export default defineNuxtConfig({
+  ssr: false,
   modules: ["@nuxt/ui", "@nuxtjs/supabase", "@vueuse/nuxt", "nuxt-primevue"],
   devtools: { enabled: true },
   debug: true,
