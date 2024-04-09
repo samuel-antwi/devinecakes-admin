f7beefc91cc3ced149dab501b29d3a61d0c1d522
Samuel Antwi <samuelantwidev@gmail.com>
2024-04-09 13:44:02 +0100

    Add user navigation logic to index page

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const user = useSupabaseUser();
+watch(
+  user,
+  () => {
+    if (user?.value) {
+      return navigateTo("/admin/content/orders");
+    } else {
+      return navigateTo("/login");
+    }
+  },
+  { immediate: true }
+);
+</script>
+<template>
+  <loading-spinner />
+</template>
